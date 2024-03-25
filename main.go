266517ecbcf7cf8302877e9e45e4637eeb98c00a
endikallanomatxin <endikallanomatxin@gmail.com>
2024-03-25 02:08:05 +0100

    structure project

--- main.go
+++ main.go
@@ -1,26 +1,17 @@
 package main
 
 import (
-    "database/sql"
     "fmt"
     "net/http"
-    "os"
 
-    _ "github.com/lib/pq"
+	"servidor/db"
 )
 
 func main() {
-    connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-        os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
-
-    db, err := sql.Open("postgres", connStr)
-    if err != nil {
-        panic(err)
-    }
-    defer db.Close()
+    db.InitializeDB()
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "index.html")
+        http.ServeFile(w, r, "web/index.html")
     })
 
     fmt.Println("El servidor está escuchando en http://localhost:8080")
